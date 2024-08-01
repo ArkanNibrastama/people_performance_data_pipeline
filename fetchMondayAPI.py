9cd1633a4cc6282b4689e63695213683d994f685
ArkanNibrastama <arkan6040nibrastama@gmail.com>
2024-08-01 22:38:19 +0700

    fix date format

--- fetchMondayAPI.py
+++ fetchMondayAPI.py
@@ -3,6 +3,11 @@ from dotenv import load_dotenv
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
+from datetime import datetime, timedelta, timezone
+
+# utc+7 (follow indonesia timezone)
+date_yesterday = (datetime.now().astimezone(timezone(timedelta(hours=7)))-timedelta(days=1)).strftime("%Y-%m-%d")
+
 
 load_dotenv()
 
@@ -51,5 +56,6 @@ for i in json_res['data']['boards'][0]['items_page']['items']:
     final_res.append(tmp)
 
 df = pd.DataFrame(final_res)
-table = pa.Table.from_pandas(df)
-pq.write_table(table, 'datalake/bronze/01-08-2024-api.parquet')
+df_filtered = df.loc[df['date'] == date_yesterday]
+table = pa.Table.from_pandas(df_filtered)
+pq.write_table(table, f'datalake/bronze/{datetime.now()}-api.parquet')
--- ingestGsheet.py
+++ ingestGsheet.py
@@ -10,9 +10,9 @@ load_dotenv()
 df = pd.read_csv(os.getenv("GSHEET_URL"))
 
 # utc+7 (follow indonesia timezone)
-date_yesterday = (datetime.now().astimezone(timezone(timedelta(hours=7)))-timedelta(days=1)).strftime("%d-%m-%Y")
+date_yesterday = (datetime.now().astimezone(timezone(timedelta(hours=7)))-timedelta(days=1)).strftime("%Y-%m-%d")
 
 df_filtered = df.loc[df['date'] == date_yesterday]
 
 table = pa.Table.from_pandas(df_filtered)
-pq.write_table(table, "datalake/bronze/01-08-2024-gsheet.parquet")  
+pq.write_table(table, f"datalake/bronze/{datetime.now()}-gsheet.parquet")  
